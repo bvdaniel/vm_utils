6921e4456fb1803ef8ea959a3b75cbe143464a98
Daniel Esteban Beltrán Varas <70292984+bvdaniel@users.noreply.github.com>
2022-08-24 16:21:29 -0400

    Update ask_nginx_title.sh

--- ask_nginx_title.sh
+++ ask_nginx_title.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 while true; do curl -s $URL | grep -oP "<title>.*</title>"; \
 sleep .1s;done
+
+// o para ir a una ruta de la url como "/random-error" 
+while true; \
+do curl -s $URL/random-error \
+-w '\n' ;sleep .1s;done
